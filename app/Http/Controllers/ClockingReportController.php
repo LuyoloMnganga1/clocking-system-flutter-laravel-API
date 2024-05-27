31608847a56bd904777025310c69c96921c272db
Luyolo Mnganga <Luyolomnganga@gmail.com>
2024-05-27 15:41:52 +0200

    changes

---
+++ app/Http/Controllers/ClockingReportController.php
@@ -0,0 +1,20 @@
+namespace App\Http\Controllers;
+
+use App\Models\Clocking;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\ClockingsExport;
+
+class ClockingReportController extends Controller
+{
+    public function getReport()
+    {
+        $clockings = Clocking::with('user')->get();
+        return response()->json($clockings);
+    }
+
+    public function exportReport()
+    {
+        return Excel::download(new ClockingsExport, 'clockings.xlsx');
+    }
+}
